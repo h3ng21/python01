6313a7a7c4604e2fe703160ef86fc670d5ec54f6
h3ng21 <leabhengseng21@gmail.com>
2024-07-20 10:03:50 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+# x = "hello"
+# y = input("Enter value of y: ")
+# print(x+" "+y)
+# print("---Welcome Amount---")
+# qty1 = int(input("Qty1: "))
+# qty2 = int(input("Qty2: "))
+# price = float(input("Price: "))
+#
+# print("Total Qty: "+str(qty1+qty2))
+# print("Amount: "+str((qty1+qty2)*price))
+# print()
+# a = input("Enter name1: ")
+# b = input("Enter name2: ")
+# temp=a
+# a=b
+# b=temp
+# print("After Changing\na: "+a)
+# print("b: "+b)
+
+print("Welcome to the band name generator\nWhich city did you grow up in ?")
+city = input()
+pet_name = input("What is the name of a pet ?\n")
+print(f"Your band name could be {city} {pet_name}")
